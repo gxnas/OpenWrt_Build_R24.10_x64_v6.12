ff3a59d7703e1f7318a58530e8521250dddd135a
gxnas <52808122+gxnas@users.noreply.github.com>
2026-01-24 09:36:26 +0800

    添加passwall2

--- diy_script/2410_x64_test_diy-part1.sh
+++ diy_script/2410_x64_test_diy-part1.sh
@@ -20,6 +20,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+sed -i '$a src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
 
 # 添加 Lucky
 git clone  --depth=1 -b main https://github.com/gdy666/luci-app-lucky.git package/lucky
