94ce680113e1bc27cc2eb6c25b209d02cb1a6062
gxnas <52808122+gxnas@users.noreply.github.com>
2026-03-02 15:32:20 +0800

    Update 2410_x64_test_diy-part1.sh

--- diy_script/2410_x64_test_diy-part1.sh
+++ diy_script/2410_x64_test_diy-part1.sh
@@ -11,13 +11,15 @@
 #
 
 # 添加源仓库
-sed -i "/helloworld/d" feeds.conf.default
+sed -i '/helloworld/d' feeds.conf.default
+sed -i '/small/d' feeds.conf.default
+sed -i '/passwall/d' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+#sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
 # 添加 Lucky
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
