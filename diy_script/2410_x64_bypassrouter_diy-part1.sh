1eb524ec07659e659b81920175950629525f95fc
gxnas <52808122+gxnas@users.noreply.github.com>
2026-09-01 23:42:19 +0800

    Update 2410_x64_bypassrouter_diy-part1.sh

--- diy_script/2410_x64_bypassrouter_diy-part1.sh
+++ diy_script/2410_x64_bypassrouter_diy-part1.sh
@@ -20,9 +20,6 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-# 添加 adguardHome
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-
 # 添加 argon 主题
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
@@ -34,12 +31,6 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 添加 netdata
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-
-# 添加 oaf
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
-# 添加 openclaw
-git clone https://github.com/10000ge10000/luci-app-openclaw.git package/luci-app-openclaw
 
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
