55c3801344304fe6fb8998d1416c67084ee5e2b6
gxnas <52808122+gxnas@users.noreply.github.com>
2026-09-22 10:47:52 +0800

    修复iStore显示为中文

--- diy_script/2410_x64_test_diy-part2.sh
+++ diy_script/2410_x64_test_diy-part2.sh
@@ -101,15 +101,6 @@ sed -i '/luci-i18n-upnp/d' package/Makefile
 sed -i '/miniupnpd/d' package/Makefile
 rm -f tmp/.package_install
 
-# 修复 iStore 默认显示为中文
-echo ">>> Enable iStore and Chinese translation"
-sed -i '/^CONFIG_PACKAGE_luci-app-store=/d' .config
-sed -i '/^CONFIG_PACKAGE_luci-app-store-i18n-zh-cn=/d' .config
-sed -i '/^CONFIG_PACKAGE_luci-i18n-base-zh-cn=/d' .config
-echo 'CONFIG_PACKAGE_luci-app-store=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-store-i18n-zh-cn=y' >> .config
-echo 'CONFIG_PACKAGE_luci-i18n-base-zh-cn=y' >> .config
-
 # 修复 default-settings 问题
 echo ">>> Purge default-settings (all variants)"
 find package/feeds -maxdepth 2 -type d -name "default-settings*" -exec rm -rf {} +
